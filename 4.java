7f23a888ab74b772e590d16a8068e6c7b428db87
mann57102-lang <mann57102@gmail.com>
2026-07-10 01:15:04 +0530

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,19 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("How to Make Tea");
+        makeTea();
+
+    }
+
+    public static void makeTea() {
+
+        System.out.println("1. Boil a cup of water.");
+        System.out.println("2. Add tea leaves and sugar.");
+        System.out.println("3. Pour in milk and boil for 2 minutes.");
+        System.out.println("4. Strain the tea into a cup.");
+        System.out.println("5. Enjoy your hot tea!");
+
+    }
+}
